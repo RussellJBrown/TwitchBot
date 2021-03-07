bde0f39321650b04642d1158086cddba0a116829
Russell Brown <33341699+RussellJBrown@users.noreply.github.com>
2021-03-06 23:21:12 -0800

    Have query returning data now.
    I need to figure out how to read the data but js is now returning the table properly. The problem was the method calling it needed an await.

--- src/app.js
+++ src/app.js
@@ -4,75 +4,41 @@ import { BOT_USERNAME , OAUTH_TOKEN, CHANNEL_NAME, BLOCKED_WORDS } from './const
 console.log("app started")
 
 const options = {
-	options: { debug: true },
-	connection: {
+  options: { debug: true },
+  connection: {
     reconnect: true,
     secure: true,
     timeout: 180000,
     reconnectDecay: 1.4,
     reconnectInterval: 1000,
-	},
-	identity: {
-		username: BOT_USERNAME,
-		password: OAUTH_TOKEN
-	},
-	channels: [ CHANNEL_NAME ]
+  },
+  identity: {
+    username: BOT_USERNAME,
+    password: OAUTH_TOKEN
+  },
+  channels: [ CHANNEL_NAME ]
 }
 
+
+const {Pool} = require('pg')
+const pool = new Pool({
+  user:"",
+  host:"localhost",
+  database:"twitchdatabase",
+  password:"",
+  port:5432
+})
+
 var pg = require('pg');
-var yourGlobalVariable;
+var betsOpen = false;
+var currentBet = "No Current Bets";
 
 const client = new tmi.Client(options)
 
 client.connect()
 
-// events
-client.on('disconnected', (reason) => {
-  onDisconnectedHandler(reason)
-})
-
-client.on('connected', (address, port) => {
-  onConnectedHandler(address, port)
-})
-
-client.on('hosted', (channel, username, viewers, autohost) => {
-  onHostedHandler(channel, username, viewers, autohost)
-})
-
-client.on('subscription', (channel, username, method, message, userstate) => {
-  onSubscriptionHandler(channel, username, method, message, userstate)
-})
-
-client.on('raided', (channel, username, viewers) => {
-  onRaidedHandler(channel, username, viewers)
-})
-
-client.on('cheer', (channel, userstate, message) => {
-  onCheerHandler(channel, userstate, message)
-})
-
-client.on('giftpaidupgrade', (channel, username, sender, userstate) => {
-  onGiftPaidUpgradeHandler(channel, username, sender, userstate)
-})
-
-client.on('hosting', (channel, target, viewers) => {
-  onHostingHandler(channel, target, viewers)
-})
-
-client.on('reconnect', () => {
-  reconnectHandler()
-})
-
-client.on('resub', (channel, username, months, message, userstate, methods) => {
-  resubHandler(channel, username, months, message, userstate, methods)
-})
-
-client.on('subgift', (channel, username, streakMonths, recipient, methods, userstate) => {
-  subGiftHandler(channel, username, streakMonths, recipient, methods, userstate)
-})
 
 // event handlers
-
 client.on('message', (channel, userstate, message, self) => {
   if(self) {
     return
@@ -83,65 +49,70 @@ client.on('message', (channel, userstate, message, self) => {
     return
   }
 
-	if(message.toLowerCase() === '!hello') {
-    hello(channel, userstate);
-    return;
+  if(message.toLowerCase() === '!hello') {
+    hello(channel, userstate)
+    return
   }
+
 
   if(message.toLowerCase()==='!play'){
-    play(channel,userstate);
-    return;
+    play(channel,userstate)
+    return
   }
 
-  if(message.toLowerCase()==='!adopt'){
-    adopt(channel,userstate);
-    return;
-  }
-
-  if(message.toLowerCase()==='!disown'){
-    disOwnRock(channel,userstate);
-    return;
-  }
-
-  if(message.toLowerCase()==='!coinflip'){
-    cointflip(channel,usertate);
-    return;
-  }
-
-  if(message.toLowerCase()==='!bankAmount'){
-    bankAmount(userstate);
-    return;
+  if(message.toLowerCase()==='!test'){
+    testDatabase(channel,userstate,message,self)
+    return
   }
 
 
-  if(message.toLowerCase()==='!wagerSetup'){
-    wagerSetup(channel,userstate);
+  if(message.toLowerCase()=== '!join'){
+    Join(channel,userstate)
+    return
+  }
+
+  if(message.toLowerCase().includes('!coinflip')){
+    console.log("Input was Coin Flip")
+    currentBet = message.replace('!coinflip','');
+    coinFlip(userstate,currentBet);
+    return;  
+  }
+
+  if(message.toLowerCase().includes('!bankAmount')){   
+    bankAmount(channel,message,userstate);
     return;
   }
+  
+
+  if(message.toLowerCase()==='!open'){
+    openbets();
+  }
+
+  if(message.toLowerCase()==='!close'){
+    closebets();
+  }
+
+
 
   if(message.toLowerCase()==='!bet1'){
-    bet1(channel,userstate);
+    bet1(channel,message,userstate);
     return;
   }
 
   if(message.toLowerCase()==='!bet2'){
-    bet2(channel,userstate);
+    bet2(channel,message,userstate);
     return;
   }
 
-  if(message.toLowerCase()==='!payout1'){
+  if(message.toLowerCase()==='!payout1'){ 
     payout1(channel,userstate);
     return;
   }
 
   if(message.toLowerCase()==='!payout2'){
-    payout2(channel,userstate);
+    payout2(channel,message,userstate);
     return;
-  }
-
-
-
-
+  }  
 
   onMessageHandler(channel, userstate, message, self)
 })
@@ -150,108 +121,22 @@ function onMessageHandler (channel, userstate, message, self) {
   checkTwitchChat(userstate, message, channel)
 }
 
-function onDisconnectedHandler(reason) {
-  console.log(`Disconnected: ${reason}`)
-}
-
-function onConnectedHandler(address, port) {
-  console.log(`Connected: ${address}:${port}`)
-}
-
-function onHostedHandler (channel, username, viewers, autohost) {
-  client.say(channel,
-    `Thank you @${username} for the host of ${viewers}!`
-  )
-}
-
-function onRaidedHandler(channel, username, viewers) {
-  client.say(channel,
-    `Thank you @${username} for the raid of ${viewers}!`
-  )
-}
-
-function onSubscriptionHandler(channel, username, method, message, userstate) {
-  client.say(channel,
-    `Thank you @${username} for subscribing!`
-  )
-}
-
-function onCheerHandler(channel, userstate, message)  {
-  client.say(channel,
-    `Thank you @${userstate.username} for the ${userstate.bits} bits!`
-  )
-}
-
-function onGiftPaidUpgradeHandler(channel, username, sender, userstate) {
-  client.say(channel,
-    `Thank you @${username} for continuing your gifted sub!`
-  )
-}
-
-function onHostingHandler(channel, target, viewers) {
-  client.say(channel,
-    `We are now hosting ${target} with ${viewers} viewers!`
-  )
-}
-
-function reconnectHandler () {
-  console.log('Reconnecting...')
-}
-
-function resubHandler(channel, username, months, message, userstate, methods) {
-  const cumulativeMonths = userstate['msg-param-cumulative-months']
-  client.say(channel,
-    `Thank you @${username} for the ${cumulativeMonths} sub!`
-  )
-}
-
-function subGiftHandler(channel, username, streakMonths, recipient, methods, userstate) {
-
-  client.say(channel,
-    `Thank you @${username} for gifting a sub to ${recipient}}.`
-  )
-
-  // this comes back as a boolean from twitch, disabling for now
-  // "msg-param-sender-count": false
-  // const senderCount =  ~~userstate["msg-param-sender-count"];
-  // client.say(channel,
-  //   `${username} has gifted ${senderCount} subs!`
-  // )
-}
-
 // commands
 
-function hello (channel, userstate) {
-  client.say(channel, `@${userstate.username}, heya!`)
+function gamble(channel,message,userstate){
+  currentBet = message.replace('!gamble','');
+  openbets();
 }
 
-function play(channel,userstate){
-  client.say(channel, `@${userstate.username}, What game do you want to play?`)
-}
+async function coinFlip(userstate,wager){
+    console.log("Create CoinFlip Query")
+    var query = "Select money from TwitchBank where username = '" + userstate.username + "' ;";
+    console.log(query)
+    await connectToDB(query);
+    //var value=0;
 
-function adopt(channel,userstate){
-  client.say(channel, `@${userstate.username}, Do you wnat to adopt a pet rock?`)
-}
-
-function adoptRock(userstate){
-  //Figure Out What these values are other than username
-  var query = "Insert from RockOwners values('" +userstate.username+"',0,0,0);"
-  connectToDB(query);
-
-}
-
-function disOwnRock(userstate){
-  var query = "Delete from RockOwners where Owners = " +  userstate.username;
-  connectToDB(query);
-
-}
-
-function coinFlip(userstate,wager){
-  var query = "Select Money from Bank where Owners = " + userstate;
-  var value=0;
-
-  if (value<wager){
-    client.say(channel, `@${userstate.username}, You don't have enough points to make that wager`)
+  if (value<parseInt(wager)){
+    client.say(channel, `@${userstate.username}, You don't have enough points to make that wager`);   
   }
 
   else{
@@ -260,128 +145,155 @@ function coinFlip(userstate,wager){
       if (number<0.5){
         client.say(channel,`@${userstate.username}, You lost the points you bet`);
         var newValue = value-wager;
-        var query = "UPDATE courses SET Money = '" + userstate.username +"'" + " WHERE Money = " + newValue + ";";
+        var query = "Update TwitchBank set Money = Money - " + wager + " where Username"  + " = " + userstate.name + ";";
         connectToDB(query);
       }
 
       else{
         client.say(channel,`@${userstate.username}, You won the points you bet`);
         var newValue = value+wager;
-        var query = "UPDATE courses SET Money = '" + userstate.username +"'" + " WHERE Money = " + newValue + ";";
+        var query = "Update TwitchBank set Money = Money + " + wager + " where Username" + " = " + userstate.name + ";"
         connectToDB(query)
       }
   }
 }
 
 function bankAmount(userstate){
-  var query = "Select Money from Bank where Owners = " + userstate;
+  var query = "Select Money from TwitchBank where Username = " + userstate.username+";";
   connectToDB(query);
-  value = 0;
   client.say(channel,`@${userstate.username}, has `+ value)
 }
 
-function wagerSetup(channel,userstate){
-  var query = "DELETE FROM Wager;";
-  var fields = input.split(username.message);
-  
-}
 
-function bet1(channel,userstate){
-  var query = "take points from bank";
-  var query2 = "put points into wager bank";
+function bet1(channel,userstate){ 
+  var query = "Select SUM(Wager) from TwitchBank where betNumber = 1;";
+  var query2 = "Select SUM(Wager) from TwitchBank where betNumber = 2;";
+  results = connectToDB(channel,query);
+  results2 = connectToDB(channel,query2);
+  payoutAdd = results/(results+results2);
+  payoutMinus = results2/(resultls2+results);
+  var query = "UPDATE TwitchBank set Money = Money " + payoutAdd + "where betNumber = 1;";
+  //var query2 = "UPDATE Money = oney " - payoutMinus + "where betNumber = 2;";
+  connectToDB(query);
+  connectToDB(query2);
+  var ClearBets = "UPDATE TwitchBank set betNumber = 0;";
+  connectToDB(ClearBets);
+  var query3 = "UPDATE TwitchBank set Wager = 0;";
+  connectToDB(query);
 
-}
+}    
 
 function bet2(channel,userstate){
-  var query = "take points from bank";
-  var query2 = "take points from wager bank"
-
+  var query = "Select SUM(Wager) from TwitchBank where betNumber = 1;";
+  var query2 = "Select SUM(Wager) from TwitchBank where betNumber = 2;";
+  results = connectToDB(channel,query);
+  results2 = connectToDB(channel,query2);
+  payoutAdd = results/(results+results2);
+  payoutMinus = results2/(resultls2+results);
+  //var query = "UPDATE Money = money" - payoutAdd + "where betNumber = 1;";
+  var query2 = "UPDATE TwitchBank set Money = Money" + payoutMinus + "where betNumber = 2;";
+  connectToDB(query);
+  connectToDB(query2);
+  var ClearBets = "UPDATE TwitchBank set betNumber = 0;";
+  connectToDB(ClearBets);
+  var query3 = "UPDATE TwitchBank set Wager = 0;";
+  connectToDB(query);
 }
 
-function payout1(channel,userstate){
-    var allWinners = "Select Users where Bet = 1";
-    var winners = connectToDB(allWinners);
-    var i;
-    var a = "Select all(first) from Wager;"
-    var b = "Select all(second) from Wager;"
-    var odds = a/(a+b);
-    for(i;i<winners.lenght();i++)
-    {
-      var query = "Update Bank Set Money = "+ wager/odds +" from Bank where Person = " + winner+";";
-      connectToDB(query);
-    }
+function wager1(channel,message,userstate){
+  var messUser = message.split(" ");
+  var messageUser = messUser[1];
+  var query = "Update TwitchBank set betNumber = 1 where Username = "+userstate.name+";";
+  var query2 = "Update TwitchBank set Money = Money - " + messageUser + " where Username = "+userstate.name+";";
+  var query3 = "Update TwitchBank set Wager = Wager " + messageUser+ " where Username = "+ userstate.name+";";
+  connectToDB(channel,query);
+  connectToDB(channel,query2);
+  connection(channel,query3);
+  currentBet = "No Current Bets";
+}
+
+function wager2(channel,userstate){
+  var messUser = message.split(" ");
+  var messageUser = messUser[1];
+  var query = "Update TwitchBank set betNumber = 2 where Username = "+userstate.name+";";
+  var query2 = "Update TwitchBank set Money = Money - " + messageUser+ " where Username = "+userstate.name+";";
+  var query3 = "Update TwitchBank set Wager = Wager " + messageUser+ " where Username = "+ userstate.name+";";
+  connectToDB(channel,query);
+  connectToDB(channel,query2);
+  connectToDB(channnel,query3);
+  currentBet = "No Current Bets";
+}
+
+//Needs to verify that only channel owner can do this command
+function openbets(){
+  betsOpen = true;
+}
+
+function closebets(){
+  betsOpen = false;
+}
+
+function testDatabase(channel,userstate){
+    var query = "INSERT into TwitchBank (username,money,wager,betNumber) values('rokruss',10000,0,0);";     
+    connectToDB(channel,query);
+}
+
+function Join(channel,userstate){
+  var query = "INSERT into TwitchBank (Username,Money,Wager,betNumber) values('"+userstate.username+"',10000,0,0);";
+  connectToDB(channel,query)
+}
+
+async function connectToDB(query2){
+  
+  try{
     
-}
+    await pool.connect()
+    console.log("Connected Successfully")
 
-function paytout2(channel,userstate){
-  var allWinners = "Select Users where Bet = 2";
-  var winners = connectToDB(allWinners);  
-  var i;
+    const {rows} = await pool.query(query2);
+    console.table(rows);
+  }
 
-  var a = "Select all(second) from Wager;";
-  var b = "Select all(first) from Wager;";
-  var odds = a/(a+b);
+  catch(ex){
+    console.log("something went wrong");
+  }
 
-  for(i;i<winners.length();i++){
-    var winner = winners[i];
-    var wager = wagers[i];
-    var query = "Update Bank Set Money = "+  wager/odds  +" from Bank where Person = "+ winner+";";
-    connectToDB(query);
+  finally{
+    await pool.end()
+    console.log("Client Disconnected")
   }
 
 
+
+
+  /*try{
+   console.log("Attempting to do query");
+    pool.query(query2,(err,res)=>{
+      console.log(res.rows)
+      pool.end()
+      return res
+    })
+
+
+    console.log("Connection Ended");
+
+  }
+  catch(err){
+    console.log(channel,err);
+  }
+*/
 }
 
 
 
 
 
-
-
-
-
-
-//Add Catagories here if you want to expand catagories
-function redeemCatagories(channel,userstate){
-  var catagories = new Array();
-  catagories[0] = "Do a voice?";
-  catagories[1] = "Request Game?" 
-  catagories[2] = "Song Request?"
-  return catagories;
-}
-
-
-function whatAreRedeems(channel,userstate){
-  var catagores = redeemCatagories(channel,userstate)
-  var i;
-  var output;
-  for (i = 0; i < catagores.length; i++) {
-       output +=  String(i+1) + catagories + " "
-  } 
-    client.say(channel,output)
-}
 
 
 setInterval(function() {
   var query = 'Update Bank Set Money = Money + 5';
   connectToDB(query)
 }, 300 * 1000);
-
-
-
-
-
-
-function connectToDB(query){
-  var connectionString = "postgres://userName:password@serverName/ip:port/nameOfDatabase";
-  var pgClient = new pg.Client(connectionString);
-  pgClient.connect();
-  var query = pgClient.query(query);
-  pgClient.end();
-
-}
-
-
 
 
 function checkTwitchChat(userstate, message, channel) {
