aff2707f372a903d30d3c227376d4a8ac9dbce1f
Russell Brown <33341699+RussellJBrown@users.noreply.github.com>
2021-03-07 20:58:44 -0800

    Delete constants.js

--- src/constants.js
+++
@@ -1,7 +0,0 @@
-export const CHANNEL_NAME = '#RokRuss';
-export const OAUTH_TOKEN = 'oauth:rfwudmpr1xsaz8vi28f55zbga1ml55';
-export const BOT_USERNAME = 'RokRuss';
-export const BLOCKED_WORDS = [
-  'cats',
-  'dogs',
-]
